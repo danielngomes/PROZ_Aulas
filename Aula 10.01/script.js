4f9aa8f1bc2eb025fae59f4ee7e6de71398b0490
danielngomes <daniel.nghomes@gmail.com>
2023-01-10 20:55:23 -0300

    Atualizado o arquivo script.js

--- Aula 10.01/script.js
+++ Aula 10.01/script.js
@@ -1,6 +1,25 @@
-const linkPerfil = document.getElementById("link-perfil");
-const navPerfil = document.getElementById("nav-perfil");
+const linkPerfil = document.getElementById("link-perfil")
+const navPerfil = document.getElementById("nav-perfil")
+const linkDados = document.getElementById("link-perfil-dados")
+const linkHome = document.getElementById("link-home")
 
-linkPerfil.addEventListener("mouseover", ()=> {
-  navPerfil.style.display = "block"
+window.addEventListener("keyup", (e)=> {  
+  
+  if(e.key === "1") {
+    if (navPerfil.style.display == "block") {
+      linkDados.click()
+    } else {
+      navPerfil.style.display = "block"
+    }
+  }
+  
+  if(e.key === "Escape") {
+    navPerfil.style.display = "none"
+  }
+
+  if(e.key === "Backspace") {
+    linkHome.click()
+  }
+  
 })
+
